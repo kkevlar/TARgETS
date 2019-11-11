176092af353ec11da1abd023ea85eebb1e730c8f
Kevin Kellar <kellar.kevin@gmail.com>
2019-11-11 12:36:12 -0800

    added C message module2

--- src/message.cpp
+++ src/message.cpp
@@ -4,17 +4,22 @@
 #include <stdio.h>
 
 
-static void (*handler_table[0x100])(MessageContext, uint8_t*, int);
+static void (*handler_table[0x100])(MessageContext*, uint8_t*, int);
 static MessageContext* mycontext;
 
 void handleMessage(MessageId id, uint8_t* message, int length)
 {
-
+	(*handler_table[id])(mycontext, message, length);
 }
 
-void defaultHandler(MessageContext context, uint8_t* data, int length)
+void handlerDefault(MessageContext* context, uint8_t* data, int length)
 {
-	fprintf(stderr, "Ignoring message of %d bytes: this message type is unimplemented\n");
+	fprintf(stderr, "Ignoring message of %d bytes: this message type is unimplemented\n", length);
+}
+
+void handlerAddBox(MessageContext* context, uint8_t* data, int length)
+{
+	fprintf(stderr, "Ignoring message of %d bytes: this message type is unimplemented\n", length);
 }
 
 void initMessageHandler(MessageContext* context)
@@ -24,8 +29,10 @@ void initMessageHandler(MessageContext* context)
 
 	for (i = 0; i < 0x100; i++)
 	{
-		handler_table[i] = &defaultHandler;
+		handler_table[i] = handlerDefault;
 	}
+
+	handler_table[MSG_ADDBOX] = handlerAddBox;
 }
 
 
