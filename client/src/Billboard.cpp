4a6c10232a51cc35d876a14a675477c0edb352ec
Kevin Kellar <kellar.kevin@gmail.com>
2019-12-11 11:52:18 -0800

    should revert to old commit

--- client/src/Billboard.cpp
+++ client/src/Billboard.cpp
@@ -8,14 +8,13 @@
 #include <vector>
 #include "GLSL.h"
 #define STB_IMAGE_IMPLEMENTATION
+#include <GLFW/glfw3.h>
 #include "Cube.h"
 #include "stb_image.h"
-#include <GLFW/glfw3.h>
 
-Billboard::Billboard(){    myCubeDim = 21;
-}
+Billboard::Billboard(){    myCubeDim = 10;}
 
-  static inline float map(
+static inline float map(
     float value, float min1, float max1, float min2, float max2)
 {
     return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
@@ -39,10 +38,6 @@ void Billboard::init(std::shared_ptr<Program>& bbprog)
     rect_tex[1] = glm::vec2(1.0f / myCubeDim, 0);
     rect_tex[2] = glm::vec2(1.0f / myCubeDim, 1.0f / myCubeDim);
     rect_tex[3] = glm::vec2(0, 1.0f / myCubeDim);
-    /*rect_tex[0] = glm::vec2(0, 0);
-    rect_tex[1] = glm::vec2(1 , 0);
-    rect_tex[2] = glm::vec2(1 , 1 );
-    rect_tex[3] = glm::vec2(0, 1 );*/
 
     // generate the VAO
     glGenVertexArrays(1, &VertexArrayID);
@@ -73,6 +68,56 @@ void Billboard::init(std::shared_ptr<Program>& bbprog)
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IndexBufferId);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * 6, rect_indi,
                  GL_STATIC_DRAW);
+
+    bbCubes = std::vector<BBCube>();
+    bbCubesPost = std::vector<BBCube>();
+
+    for (int x = 0; x < myCubeDim; x++)
+    {
+        for (int y = 0; y < myCubeDim; y++)
+        {
+            BBCube cube = BBCube();
+
+            cube.target.pos = glm::vec3((x) * (1.0f / myCubeDim),
+                                        (y) * (0.5f / myCubeDim), -1.5);
+            cube.target.pos.x -= 1 / 2.0f;
+            cube.target.pos.y -= 1 / 4.0f;
+
+            cube.target.scale =
+                glm::vec3(1.0f / myCubeDim, 1.0f / myCubeDim, 1.0f / myCubeDim);
+            cube.source = cube.target;
+            // cube.source.scale *= 0.001;
+            cube.source.pos.z -= 500;
+            cube.source.pos.x = map(rand() % 1000, 0, 1000, -100, 100);
+            cube.source.pos.y = map(rand() % 1000, 0, 1000, -100, 100);
+
+            cube.phase = (1.0f - length(glm::vec2(cube.target.pos.x,
+                                                  cube.target.pos.y))) *
+                         0.5;
+            cube.phase += map(rand() % 1000, 0, 1000, -0.05, 0.05);
+            cube.phase += map(rand() % 1000, 0, 1000, 0, 0.05);
+            cube.phase += 0.4;
+            cube.resetInterp();
+            cube.dosin = 1;
+            cube.texOffset.x = x * (1.0f / myCubeDim);
+            cube.texOffset.y = y * (1.0f / myCubeDim);
+
+            BBCube pcube = BBCube();
+            pcube = cube;
+            pcube.source = cube.target;
+            pcube.target = pcube.source;
+            pcube.target.pos.z -= 1000;
+            // pcube.target.scale *= 0.001f;
+            pcube.dosin = 1;
+            pcube.phase = map(1 - glm::abs(cube.target.pos.x), 0, 1, -0.2, 0);
+            pcube.phase += map(rand() % 1000, 0, 1000, -0.01, 0.01);
+            pcube.resetInterp();
+
+            bbCubes.push_back(cube);
+            bbCubesPost.push_back(pcube);
+        }
+    }
+
     glBindVertexArray(0);
 
     int width, height, channels;
@@ -184,8 +229,8 @@ void Billboard::draw(std::shared_ptr<Program>& bbprog,
     w += frametime;
 
     glm::vec4 l1p = glm::vec4(0, 0, -5, 0);
-    l1p = glm::rotate(glm::mat4(1), sin(w*2)/2.0f, glm::vec3(0, 1, 0)) * l1p;
-    
+    l1p =
+        glm::rotate(glm::mat4(1), sin(w * 2) / 2.0f, glm::vec3(0, 1, 0)) * l1p;
 
     glUniform3f(bbprog->getUniform("light1pos"), l1p.x, l1p.y, l1p.z);
 
@@ -194,23 +239,21 @@ void Billboard::draw(std::shared_ptr<Program>& bbprog,
     Vi[1][3] = 0;
     Vi[2][3] = 0;
 
-	
     glUniformMatrix4fv(bbprog->getUniform("V"), 1, GL_FALSE, &V[0][0]);
     glUniformMatrix4fv(bbprog->getUniform("P"), 1, GL_FALSE, &P[0][0]);
 
-
-	if (glfwGetTime() < 12 )
-    for (int i = 0; i < bbCubes.size(); i++)
-    {
-        BBCube& cube = bbCubes.data()[i];
-        cube.interp += frametime*0.045;
-        cube.interpBetween();
-        glUniform2f(bbprog->getUniform("texOffset"), cube.texOffset.x,
-                    cube.texOffset.y);
-        cube.sendModelMatrix(bbprog, glm::mat4(1));
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
-    }
-        else
+    if (glfwGetTime() < 12)
+        for (int i = 0; i < bbCubes.size(); i++)
+        {
+            BBCube& cube = bbCubes.data()[i];
+            cube.interp += frametime * 0.045;
+            cube.interpBetween();
+            glUniform2f(bbprog->getUniform("texOffset"), cube.texOffset.x,
+                        cube.texOffset.y);
+            cube.sendModelMatrix(bbprog, glm::mat4(1));
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
+        }
+    else
         for (int i = 0; i < bbCubesPost.size(); i++)
         {
             BBCube& cube = bbCubesPost.data()[i];
@@ -221,7 +264,6 @@ void Billboard::draw(std::shared_ptr<Program>& bbprog,
             cube.sendModelMatrix(bbprog, glm::mat4(1));
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
         }
-
 
     glBindVertexArray(0);
 
